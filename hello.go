81b882a2abe651c2e0ff20b5e8ac6dbc239cdf6b
metatronz <32549660+metatronz@users.noreply.github.com>
2019-11-19 10:17:34 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -4,17 +4,6 @@ import (
 	"fmt"
 )
 
-var problem = [][]byte{
-	[]byte("...?.....?............#"),
-	[]byte("......?.......?....####"),
-	[]byte(".?........?.....#####?#"),
-	[]byte(".......?.....#####?####"),
-	[]byte("..?..?....#############"),
-	[]byte(".......######?######?##"),
-	[]byte("....#####?#######?#####"),
-	[]byte(".###################?##"),
-}
-
 // var problem = [][]byte{
 // []byte(".###################?##"),
 // []byte("....#####?#######?#####"),
@@ -26,34 +15,37 @@ var problem = [][]byte{
 // []byte("...?.....?............#"),
 // }
 
-var wx, wy, wb float32
+//var wx, wy, wb float32
 
-func neuron(x, y, bias int) int {
-	f := wx*float32(x) + wy*float32(y) + wb*float32(bias)
+//Weights of neuron
+type Weights struct{ wx, wy, wb float32 }
+
+func neuron(x, y, bias int, w *Weights) int {
+	f := w.wx*float32(x) + w.wy*float32(y) + w.wb*float32(bias)
 	if f < 0 {
 		return -1
 	}
 	return 1
 }
 
-func learn(x, y, desired int) {
+func learn(x, y, desired int, w *Weights) {
 	const learningConst float32 = 0.1
-	err := desired - neuron(x, y, 1)
-	wx += float32(err*x) * learningConst
-	wy += float32(err*y) * learningConst
-	wb += float32(err) * 1 * learningConst
+	err := desired - neuron(x, y, 1, w)
+	w.wx += float32(err*x) * learningConst
+	w.wy += float32(err*y) * learningConst
+	w.wb += float32(err) * 1 * learningConst
 }
 
-func stepOfLearning() {
+func stepOfLearning(problem [][]byte, w *Weights) {
 	y := 0
 	for line := range problem {
 		x := 0
 		for b := range problem[line] {
 			if problem[line][b] == byte('.') {
-				learn(x, y, -1)
+				learn(x, y, -1, w)
 			}
 			if problem[line][b] == byte('#') {
-				learn(x, y, 1)
+				learn(x, y, 1, w)
 			}
 			x++
 		}
@@ -61,23 +53,27 @@ func stepOfLearning() {
 	}
 }
 
-func solution() {
+func printSolution(problem [][]byte, w *Weights) {
 	y := 0
-
+	cp := make([]byte, len(problem[0]))
 	for line := range problem {
 		x := 0
-		var cp []byte
+		//var cp []byte
+
 		for b := range problem[line] {
 			if problem[line][b] == '?' {
-				r := neuron(x, y, 1)
+				r := neuron(x, y, 1, w)
 				if r < 0 {
-					cp = append(cp, '!')
+					//cp = append(cp, '!')
+					cp[x] = '!'
 				} else {
-					cp = append(cp, '%')
+					//cp = append(cp, '%')
+					cp[x] = '%'
 				}
 
 			} else {
-				cp = append(cp, problem[line][b])
+				//cp = append(cp, problem[line][b])
+				cp[x] = problem[line][b]
 			}
 			x++
 
@@ -90,14 +86,125 @@ func solution() {
 
 func main() {
 
+	var problem = [][]byte{
+		[]byte("...?.....?............#"),
+		[]byte("......?.......?....####"),
+		[]byte(".?........?.....#####?#"),
+		[]byte(".......?.....#####?####"),
+		[]byte("..?..?....#############"),
+		[]byte(".......######?######?##"),
+		[]byte("....#####?#######?#####"),
+		[]byte(".###################?##"),
+	}
+
+	var w = &Weights{}
+
 	x := 0
 	for x < 100 {
-		stepOfLearning()
-		solution()
+		stepOfLearning(problem, w)
+		printSolution(problem, w)
 		x++
 		fmt.Println(x)
 	}
 
-	fmt.Printf("wx = %f,wy = %f,wb = %f\n", wx, wy, wb)
+	fmt.Printf("wx = %f,wy = %f,wb = %f\n", w.wx, w.wy, w.wb)
 
 }
+
+
+
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// var problem = [][]byte{
+// 	[]byte("...?.....?............#"),
+// 	[]byte("......?.......?....####"),
+// 	[]byte(".?........?.....#####?#"),
+// 	[]byte(".......?.....#####?####"),
+// 	[]byte("..?..?....#############"),
+// 	[]byte(".......######?######?##"),
+// 	[]byte("....#####?#######?#####"),
+// 	[]byte(".###################?##"),
+// }
+
+
+
+// var wx, wy, wb float32
+
+// func neuron(x, y, bias int) int {
+// 	f := wx*float32(x) + wy*float32(y) + wb*float32(bias)
+// 	if f < 0 {
+// 		return -1
+// 	}
+// 	return 1
+// }
+
+// func learn(x, y, desired int) {
+// 	const learningConst float32 = 0.1
+// 	err := desired - neuron(x, y, 1)
+// 	wx += float32(err*x) * learningConst
+// 	wy += float32(err*y) * learningConst
+// 	wb += float32(err) * 1 * learningConst
+// }
+
+// func stepOfLearning() {
+// 	y := 0
+// 	for line := range problem {
+// 		x := 0
+// 		for b := range problem[line] {
+// 			if problem[line][b] == byte('.') {
+// 				learn(x, y, -1)
+// 			}
+// 			if problem[line][b] == byte('#') {
+// 				learn(x, y, 1)
+// 			}
+// 			x++
+// 		}
+// 		y++
+// 	}
+// }
+
+// func solution() {
+// 	y := 0
+
+// 	for line := range problem {
+// 		x := 0
+// 		var cp []byte
+// 		for b := range problem[line] {
+// 			if problem[line][b] == '?' {
+// 				r := neuron(x, y, 1)
+// 				if r < 0 {
+// 					cp = append(cp, '!')
+// 				} else {
+// 					cp = append(cp, '%')
+// 				}
+
+// 			} else {
+// 				cp = append(cp, problem[line][b])
+// 			}
+// 			x++
+
+// 		}
+
+// 		y++
+// 		fmt.Println(string(cp))
+// 	}
+// }
+
+// func main() {
+
+// 	x := 0
+// 	for x < 100 {
+// 		stepOfLearning()
+// 		solution()
+// 		x++
+// 		fmt.Println(x)
+// 	}
+
+// 	fmt.Printf("wx = %f,wy = %f,wb = %f\n", wx, wy, wb)
+
+// }
